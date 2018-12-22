58da3ec5eaa20520bd204b1967b09175ea683d7b
Agnius Pažečka <agnius@agniuss-macbook-pro.local>
2018-12-22 16:10:10 +0200

    Initial Commit

---
+++ TimeCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeCounter
+//
+//  Created by Agnius Pazecka on 22/12/2018.
+//  Copyright © 2018 Agnius Pazecka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
